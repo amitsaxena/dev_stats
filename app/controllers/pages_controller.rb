320949ebbfed387d7775aaf29de6a91e931ac5fc
amitsaxena <amit@mazdigital.com>
2017-01-24 03:54:29 +0530

    Basic email error handling and some structure.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
   
-  def index
-    
+  
+  def result
+    render "error.js.erb" if params[:email].blank? || params[:email] !~ /\A(\S+)@(.+)\.(\S+)\z/
+    if !params[:so_username].blank?
+      
+    end
+    if !params[:github_username].blank? || params[:email].blank?
+      
+    end
   end
   
 end
